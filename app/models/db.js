9d5aa30e20196b17a7efa85ae0ff14e4e510584e
AlvaroSanchezDomingo <alvaro.sanchez.domingo@gmail.com>
2021-05-13 19:02:03 +0200

    Cloud atlas

--- app/models/db.js
+++ app/models/db.js
@@ -16,7 +16,7 @@ async function seed() {
   const data = require('./seed-data.json');
   const Parking = require('./parking');
   const User = require('./user');
-  const dbData = await seeder.seed(data, { dropDatabase: false, dropCollections: true });
+  const dbData = await seeder.seed(data, { dropDatabase: false, dropCollections: false });
   console.log(dbData);
 }
 
